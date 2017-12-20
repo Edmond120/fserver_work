7434fcb6664cedacf05cc82239760338423be5d5
edmond120 <edmondwong888@msn.com>
2017-12-19 21:48:00 -0500

    signal handling and operation to string

--- forking_server.c
+++ forking_server.c
@@ -1,5 +1,6 @@
 #include "pipe_networking.h"
 #include <signal.h>
+#include <string.h>
 
 void process(char *s);
 void subserver(int from_client);
@@ -12,6 +13,7 @@ static void sighandler(int signo) {
 }
 
 int main() {
+  signal(SIGINT,sighandler);
   while(1){
     //Blocks Until Client Connects
     int from_client = server_setup();
@@ -34,11 +36,20 @@ void subserver(int from_client) {
   char* buff = (char*) calloc((BUFFER_SIZE / sizeof(char)), sizeof(char));
   while(read(from_client, buff, BUFFER_SIZE)){
     printf("[subserver] recieved: [%s]\n", buff);
-    /* process(buff); */
+    process(buff);
     write(to_client, buff, BUFFER_SIZE);
   }
 }
 
-void process(char * s) {
-
+void process(char * s) {//reverses string
+	int len = strlen(s);
+	int start = 0;
+	int end = len - 1;
+	char tmp;
+	printf("len: %d\n",len);
+	while(start < end){
+		tmp = s[start];
+		s[start++] = s[end];
+		s[end--] = tmp;
+	}
 }
